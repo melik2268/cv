303fcfd122a8160082d4deec0c896829ce35d239
Melik Bağrıyanık <122457801+melik2268@users.noreply.github.com>
2023-03-09 20:42:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ AOS.init();
 // You can also pass an optional settings object
 // below listed default settings
 AOS.init({
-  
+
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
@@ -13,3 +13,12 @@ AOS.init({
   anchorPlacement: AbortController, // defines which position of the element regarding to window should trigger the animation
 
 });
+// multiple text
+const typed = new Typed('.multiple-text', {
+  strings: ["", "Frontend Developer", "Mobile Application Developer", "Web Designer"],
+  typeSpeed: 100,
+  backSpeer: 100,
+  backDelay: 100,
+  loop: true
+
+})
